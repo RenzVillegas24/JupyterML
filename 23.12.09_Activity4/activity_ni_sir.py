29a19d9d154f382a032e022cedf0e3ce5939555c
RenzVillegas24 <renzjustinevillegas@gmail.com>
2024-01-04 14:36:04 +0800

    January 4

--- 23.12.09_Activity4/activity_ni_sir.py
+++ 23.12.09_Activity4/activity_ni_sir.py
@@ -123,6 +123,12 @@ y_test = multiple_linear_regression.predict(input_test)
 
 # To Apply The K-fold Cross validation for the Multiple Linear Regression
 from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import explained_variance_score
+from sklearn.metrics import r2_score
+from math import sqrt
 
 k_fold = KFold(n_splits = 10, shuffle = True, random_state = 0)
 
@@ -131,93 +137,113 @@ k_fold = KFold(n_splits = 10, shuffle = True, random_state = 0)
  # B. mean_squared_error = "neg_mean_squared_error"
  # C. r-sqaured_error = "r2"
  
-from sklearn.model_selection import cross_val_score
 
 #To Feature scale the X_dummytrap_standard
 X_dummytrap_standard = X_dummytrap.copy()
-X_dummytrap_standard = standard_scaler.fit_transform(X_dummytrap_standard[:, 2:5])
+X_dummytrap_standard[:, 2:5] = standard_scaler.fit_transform(X_dummytrap_standard[:, 2:5])
+
+def multiple_linear_regression_template():
+      # A. For the mean absolute error (MAE) as scoring for cross validation
+      MAE = (cross_val_score(multiple_linear_regression, X = X_dummytrap_standard, y = Y, cv = k_fold, scoring = "neg_mean_absolute_error"))*-1
+      MAE_ave = MAE.mean()
+      MAE_dev = MAE.std()
+
+      print(" The Mean Absolute Error of KFolds:", MAE)
+      print(" ")
+
+      print(" The Average Mean Absolute Error of KFolds:", MAE_ave)
+      print(" ")
+      print(" The Standard Deviation Mean Absolute Error of KFolds:", MAE_dev)
+      print(" ")
+
+      # B. For the mean squared error (MSE) as scoring for cross validation
+
+      MSE = (cross_val_score(multiple_linear_regression, X = X_dummytrap_standard, y = Y, cv = k_fold, scoring = "neg_mean_squared_error"))*-1
+      MSE_ave = MSE.mean()
+      MSE_dev = MSE.std()
+
+      print(" The Mean Squared Error of KFolds:", MSE)
+      print(" ")
+
+      print(" The Average Mean Squared Error of KFolds:", MSE_ave)
+      print(" ")
+      print(" The Standard Deviation Mean Squared Error of KFolds:", MSE_dev)
+      print(" ")
+
+      # C. For the r-sqaured_error (R2) as scoring for cross validation
+
+      r2 = (cross_val_score(multiple_linear_regression, X= X_dummytrap_standard, y = Y, cv = k_fold, scoring = "r2"))
+      r2_ave = r2.mean()
+      r2_dev = r2.std()
+
+      print(" The Mean R-Squared Error of KFolds:", r2)
+      print(" ")
+
+      print(" The Average Mean R-Squared Error of KFolds:", r2_ave)
+      print(" ")
+      print(" The Standard Deviation Mean R-Squared Error of KFolds:", r2_dev)
+      print(" ")
+
+      #To Evaluate the performance of SLR Using Hold-Out Validation
+
+      # A. For the Mean Absolute Error (MAE)
+      MAE_holdout = mean_absolute_error(Y_test, Y_predict)
+      print("Mean Absolute Error: %4f"
+            %MAE_holdout)
+      print(" ")
+
+      # B. For Mean Squared Error (MSE) 
+      MSE_holdout = mean_squared_error(Y_test, Y_predict)
+      print("Mean Squared Error: %4f"
+            %MSE_holdout)
+      print(" ")
+
+      # C. For the Root Mean Squared Error/Deviation (RMSE/RMSD)
+      RMSE_holdout = sqrt(MSE_holdout)
+      print("Root Mean Squared Error: %4f"
+            %RMSE_holdout)
+      print(" ")
+
+      # D. For Explained Variance Score (EVS)
+      EVS_holdout = explained_variance_score(Y_test, Y_predict)
+      print("Explained Variance Score: %4f"
+            %EVS_holdout)
+      print(" ")
+
+      # E. For the Coefficient of Determination Regression Score Function (R^2)
+      r2_holdout = r2_score(Y_test, Y_predict)
+      print("R^2 Score: %4f"
+            %r2_holdout)
+      print(" ")
 
 
-# A. For the mean absolute error (MAE) as scoring for cross validation
-MAE = (cross_val_score(multiple_linear_regression, X = X_dummytrap_standard, y = Y, cv = k_fold, scoring = "neg_mean_absolute_error"))*-1
-MAE_ave = MAE.mean()
-MAE_dev = MAE.std()
+print("MODEL 1: ALL INDEPENDENT VARIABLES")
+multiple_linear_regression_template()
 
-print(" The Mean Absolute Error of KFolds:", MAE)
-print(" ")
+'''
+MODEL 2
+'''
 
-print(" The Average Mean Absolute Error of KFolds:", MAE_ave)
-print(" ")
-print(" The Standard Deviation Mean Absolute Error of KFolds:", MAE_dev)
-print(" ")
+#To Feature scale the X_dummytrap_standard
+X_dummytrap_standard = X_dummytrap.copy()
+X_dummytrap_standard[:, 2:5] = standard_scaler.fit_transform(X_dummytrap_standard[:, 2:5])
 
-# B. For the mean squared error (MSE) as scoring for cross validation
+# Get the 0th index and 2nd index of the X_dummytrap_standard
+X_dummytrap_standard = X_dummytrap_standard[:, [0, 2]]
 
-MSE = (cross_val_score(multiple_linear_regression, X = X_dummytrap_standard, y = Y, cv = k_fold, scoring = "neg_mean_squared_error"))*-1
-MSE_ave = MSE.mean()
-MSE_dev = MSE.std()
+print("MODEL 2: R&D Spend and Marketing Spend")
+multiple_linear_regression_template()
 
-print(" The Mean Squared Error of KFolds:", MSE)
-print(" ")
+'''
+MODEL 3
+'''
 
-print(" The Average Mean Squared Error of KFolds:", MSE_ave)
-print(" ")
-print(" The Standard Deviation Mean Squared Error of KFolds:", MSE_dev)
-print(" ")
+#To Feature scale the X_dummytrap_standard
+X_dummytrap_standard = X_dummytrap.copy()
+X_dummytrap_standard[:, 2:5] = standard_scaler.fit_transform(X_dummytrap_standard[:, 2:5])
 
-# C. For the r-sqaured_error (R2) as scoring for cross validation
+# Get the 0th index and 2nd index of the X_dummytrap_standard
+X_dummytrap_standard = X_dummytrap_standard[:, [0]]
 
-r2 = (cross_val_score(multiple_linear_regression, X= X_dummytrap_standard, y = Y, cv = k_fold, scoring = "r2"))
-r2_ave = r2.mean()
-r2_dev = r2.std()
-
-print(" The Mean R-Squared Error of KFolds:", r2)
-print(" ")
-
-print(" The Average Mean R-Squared Error of KFolds:", r2_ave)
-print(" ")
-print(" The Standard Deviation Mean R-Squared Error of KFolds:", r2_dev)
-print(" ")
-
-#To Evaluate the performance of SLR Using Hold-Out Validation
-
-# A. For the Mean Absolute Error (MAE)
-from sklearn.metrics import mean_absolute_error
-MAE_holdout = mean_absolute_error(Y_test, Y_predict)
-print("Mean Absolute Error: %4f"
-      %MAE_holdout)
-print(" ")
-
-# B. For Mean Squared Error (MSE) 
-from sklearn.metrics import mean_squared_error
-MSE_holdout = mean_squared_error(Y_test, Y_predict)
-print("Mean Squared Error: %4f"
-      %MSE_holdout)
-print(" ")
-
-# C. For the Root Mean Squared Error/Deviation (RMSE/RMSD)
-from math import sqrt
-RMSE_holdout = sqrt(MSE_holdout)
-print("Root Mean Squared Error: %4f"
-      %RMSE_holdout)
-print(" ")
-
-# D. For Explained Variance Score (EVS)
-from sklearn.metrics import explained_variance_score
-EVS_holdout = explained_variance_score(Y_test, Y_predict)
-print("Explained Variance Score: %4f"
-      %EVS_holdout)
-print(" ")
-
-# E. For the Coefficient of Determination Regression Score Function (R^2)
-from sklearn.metrics import r2_score
-r2_holdout = r2_score(Y_test, Y_predict)
-print("R^2 Score: %4f"
-      %r2_holdout)
-print(" ")
-
-
-
-
-
-
+print("MODEL 3: R&D Spend")
+multiple_linear_regression_template()
