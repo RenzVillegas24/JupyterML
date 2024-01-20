0706971ec22aee0db60de565fe5f0e862bd69cf6
RenzVillegas24 <renzjustinevillegas@gmail.com>
2024-01-20 09:03:32 +0800

    Hello

--- 24.01.10_Activity_1/t.py
+++ 24.01.10_Activity_1/t.py
@@ -1,23 +1,46 @@
+
+import pandas as pd
 import re
+import numpy as np
 
-def transform_backlit(description):
-    # Check for patterns and apply transformations
-    if re.search(r'\b4[-\s]?zone\b', description, re.IGNORECASE):
-        return "4-Zone RGB"
-    elif re.search(r'\bbacklit\b', description, re.IGNORECASE):
-        if re.search(r'\brgb\b', description, re.IGNORECASE):
-            return "RGB"
-        elif re.search(r'\bwhite\b', description, re.IGNORECASE):
-            return "White"
-        else:
-            return "None"
-    else:
-        return "None"
+df = pd.read_csv("complete laptop data0_utf.csv")
 
-# Test cases
-print(transform_backlit("4-zone rgb backlit"))         # Output: 4-Zone RGB
-print(transform_backlit("4 zone baclit"))              # Output: 4-Zone RGB
-print(transform_backlit("backlit, rgb"))               # Output: RGB
-print(transform_backlit("white backlit"))               # Output: White
-print(transform_backlit("None backlit keyboard"))      # Output: None
-print(transform_backlit("Random text"))                # Output: None
+def parse_name(text):
+    return re.findall(r"\b\w+\b", text.lower())
+
+'''# get all non-numeric columns
+for i in df[df.dtypes[df.dtypes == 'object'].index].columns:
+    print(i)
+    print(np.unique([parse_name(df[i][j])[0] for j in range(df.shape[0]) if type(df[i][j]) == str]))
+
+
+'''
+
+def split_space(text):
+    return re.findall(r"\b\w+\b", text)
+
+# tokenize the data, get all the unique values and store it in a list
+brand_name = np.unique([split_space(i.lower())[0] for i in df['name']])
+
+# replace the name of the brand with the brand name
+def parse_name(text):
+    text = text.lower()
+
+    for brand in brand_name:
+        if brand in split_space(text):
+            return brand
+    return text
+    
+# get the brand name from the name column
+df['Brand'] = df['name'].apply(lambda x: parse_name(x))
+
+
+c = 0.67 # for static conversion use this
+
+df['Price'] = round(df['Price'].str.replace(',', '').str.lstrip('?').astype(float) * c, 2)
+
+
+for i in df[df.dtypes[df.dtypes == 'object'].index].columns:
+    print(i)
+    print(np.unique([split_space(t.lower())[0] for t in df[i] if type(t) == str]))
+
